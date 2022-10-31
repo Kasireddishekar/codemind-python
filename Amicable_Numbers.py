e436af729d2a6b7bf96ee6e61b536db61cbc0d4f
Kasireddishekar <98140510+Kasireddishekar@users.noreply.github.com>
2022-10-31 14:35:48 +0530

    def pfs(n):     sum=0     for i in range(1,n):         if n%i==0:             sum=sum+i     return sum a=int(input()) b=int(input()) k=pfs(a) l=pfs(b) if k==b and l==a:     print("Amicable") else:     print("Not Amicable")

---
+++ Amicable_Numbers.py
@@ -0,0 +1,14 @@
+def pfs(n):
+    sum=0
+    for i in range(1,n):
+        if n%i==0:
+            sum=sum+i
+    return sum
+a=int(input())
+b=int(input())
+k=pfs(a)
+l=pfs(b)
+if k==b and l==a:
+    print("Amicable")
+else:
+    print("Not Amicable")
