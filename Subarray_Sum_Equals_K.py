b136532a43176cce68292b89df883358d1c40577
Kasireddishekar <98140510+Kasireddishekar@users.noreply.github.com>
2022-12-06 08:45:22 +0530

    n,b=map(int,input().split()) d=list(map(int,input().split())) c=0 for i in range(n):     s=0     for j in range(i,n):         s=s+d[j]         if s==b:             c=c+1 print(c)

---
+++ Subarray_Sum_Equals_K.py
@@ -0,0 +1,10 @@
+n,b=map(int,input().split())
+d=list(map(int,input().split()))
+c=0
+for i in range(n):
+    s=0
+    for j in range(i,n):
+        s=s+d[j]
+        if s==b:
+            c=c+1
+print(c)
